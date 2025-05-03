e00a264b24e714d5677a2893f3813fe31dee0b24
Paul Gierz <pgierz@awi.de>
2025-05-02 20:38:56 -0700

    repo: Pre-Commit (#4)
    * build(pyproject.toml): adds dependencies for sdist install
    
    * wip: correct the dependencies table formatting
    
    * wip... 1
    
    * wip... 2
    
    * wip: adds utility script to sync dependencies
    
    * wip: adds pre-commit and runs once
    
    * wip: checks dependency sync via git precommit hook

---
+++ dev/pyproject_check_dependencies.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Checks if the dependencies in pyproject.toml between [tool.pixi.dependencies] and [project.dependencies] sections
+are identical.
+"""
+from pathlib import Path
+
+import toml
+
+
+def check_dependencies_sync_from_pixi_and_project(pyproject_path: Path):
+    print(f"Checking dependencies in {pyproject_path}...")
+    # Load pyproject.toml
+    data = toml.load(pyproject_path)
+
+    # Get tool.pixi.dependencies
+    pixi_deps = data.get("tool", {}).get("pixi", {}).get("dependencies", {})
+    if not isinstance(pixi_deps, dict):
+        raise ValueError("Expected [tool.pixi.dependencies] to be a dict")
+
+    # Set project.dependencies to match tool.pixi.dependencies
+    project = data.setdefault("project", {})
+    project_dependencies = sorted(project["dependencies"])
+    pixi_dependencies = sorted(
+        f"{k}{v}" for k, v in pixi_deps.items() if k and v
+    )  # remove duplicates, sort for stability
+
+    if project_dependencies != pixi_dependencies:
+        raise ValueError(
+            f"Expected [project.dependencies] to match [tool.pixi.dependencies] in: {pyproject_path}"
+        )
+    print("[project.dependencies] matches [tool.pixi.dependencies].")
+
+
+if __name__ == "__main__":
+    check_dependencies_sync_from_pixi_and_project(Path("pyproject.toml"))
